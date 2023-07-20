04eef197a2e739c9f756d192c42a4ccb15d2c3c9
nehanarta <nehabaswanthrao@gmail.com>
2023-07-20 18:33:30 +0530

    Added:valid Email

--- src/main/java/org/example/UserReg.java
+++ src/main/java/org/example/UserReg.java
@@ -4,6 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserReg {
+
+
     public static String validFirstName(String fName) {
         Pattern pattern = Pattern.compile("[A-Za-z]{3,}");
 
@@ -15,16 +17,37 @@ public class UserReg {
         }
 
     }
-    public static String validLastName(String fName) {
+    public static String validLastName(String LName) {
         Pattern pattern = Pattern.compile("[A-Za-z]{3,}");
 
-        Matcher matcher = pattern.matcher(fName);
+        Matcher matcher = pattern.matcher(LName);
         if (matcher.matches()) {
             return "valid";
         } else {
             return "invalid";
         }
     }
+    public static String validEmail(String Email) {
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+
+        Matcher matcher = pattern.matcher(Email);
+        if (matcher.matches()) {
+            return "valid";
+        } else {
+            return "invalid";
+        }
+    }
+    public static String validMobileNo(String MobileNo) {
+        Pattern pattern = Pattern.compile("(0/91)?[7-9][0-9]{9}");
+
+        Matcher matcher = pattern.matcher(MobileNo);
+        if (matcher.matches()) {
+            return "valid";
+        } else {
+            return "invalid";
+        }
+    }
+
 }
 
 
--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -3,23 +3,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class Testing {
-static UserReg User;
-@BeforeAll
-    public static void initiate(){
-    User=new UserReg();
-}
-@Test
-    public void  givenTextWhenItContainsFirstCapsAndMinThreeCharShouldReturnTrue(){
-    String actual=UserReg.validFirstName("Neha");
-    String Excepted="valid";
-    Assertions.assertEquals(Excepted,actual);
-}
-@Test
-    public void givenTextWhenContainsLastCapsAndMinThreeCharShouldReturnTrue() {
-    String actual = UserReg.validFirstName("Patil");
-    String Excepted = "valid";
-    Assertions.assertEquals(Excepted, actual);
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-}
-}
+public class Testing {
+    static UserReg User;
+
+    @BeforeAll
+    public static void initiate() {
+        User = new UserReg();
+    }
+
+    @Test
+    public void givenTextWhenItContainsFirstCapsAndMinThreeCharShouldReturnTrue() {
+        String actual = UserReg.validFirstName("Neha");
+        String Excepted = "valid";
+        Assertions.assertEquals(Excepted, actual);
+    }
+
+    @Test
+    public void givenTextWhenContainsLastCapsAndMinThreeCharShouldReturnTrue() {
+        String actual = UserReg.validLastName("Patil");
+        String Excepted = "valid";
+        Assertions.assertEquals(Excepted, actual);
+
+    }
+
+    @Test
+    public void givenEmailWhenAccordingToProperNumberShouldReturnTrue() {
+        String actual = UserReg.validEmail("Abc@gmail.com");
+        String Excepted = "valid";
+        Assertions.assertEquals(Excepted, actual);
+    }
+
+    }
+
+
